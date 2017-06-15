2fc138f50de7603664eb757eb9f3a6b556d992d2
Destroyer-of-Worlds <welcometogloomworld@yandex.ru>
2017-06-15 13:36:32 +0300

    fixed interface and some others things

--- test1/mainwindow.h
+++ test1/mainwindow.h
@@ -12,7 +12,8 @@
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QDir>
-
+#include <QTableWidget>
+#include <QHeaderView>
 
 class MainWindow : public QDialog
 {
@@ -22,19 +23,22 @@ public:
     MainWindow(QWidget *parent = 0);
 
 private:
-    QLabel *lbl;
+    QLabel *lbl1;
+    QLabel *lbl2;
     QLineEdit *dirPath;
 
     QPushButton *browse;
     QPushButton *ok;
     QPushButton *quit;
 
+    QTableWidget *table;
     QTextEdit *text;
+
 private slots:
     void OkClicked();
     void TextChanged(QString str);
     void Open();
-    void start(const QDir& dir);
+    void fileStat(const QDir& dir, QMap<QString, QPair<qint64, qint32> >& fileGroups);
 };
 
 #endif // MAINWINDOW_H
